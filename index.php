e64c997a09513dbf82d8eb4c3eebf14b10d010bd
sain firstio <sainfirstio27@gmail.com>
2023-09-23 19:26:23 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Zyrtez</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Simple line icons-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.5.5/css/simple-line-icons.min.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+    <nav id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand"><a href="#page-top"></a></li>
+            <li class="sidebar-nav-item"><a href="#page-top">Home</a></li>
+            <li class="sidebar-nav-item"><a href="#about">About Me</a></li>
+            <li class="sidebar-nav-item"><a href="#portfolio">Portfolio</a></li>
+            <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+    <!-- Header-->
+    <header class="masthead d-flex align-items-center">
+        <div class="container px-4 px-lg-7">
+            <div class="row align-items-center">
+                <div class="col-lg-5 col-sm-12">
+                    <img src="./assets/img/pic1.png" alt="pic1" />
+                </div>
+                <div class="col-lg-7 col-sm-12 justify-content-center">
+                    <h1 class="mb-1 text-secondary">Sain Firstio Ansari</h1>
+                    <h3 class="mb-5 text-secondary"><em>Computer Engineer</em></h3>
+                </div>
+                <!-- <a class="btn btn-primary btn-xl" href="#about">Find Out More</a> -->
+            </div>
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+
+        </div>
+    </header>
+    <!-- About-->
+    <section class="content-section bg-secondary" id="about">
+        <div class="container px-4 px-lg-5 text-center">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-7 col-sm-12 text-secondary">
+                    <h1 class="text-primary mb-0">About Me</h1>
+                    <p class="text-white lead mb-5">
+                        Graduate of Telkom University with a final project tinkering about IoT and microcontrollers.
+                        Looking for a job which offers me the opportunity to develop new skills. Motivated to learn, and
+                        grow in information technology
+                    </p>
+                    <div class="row row-cols-md-2 row-cols-sm-1 mt-5 mx-3">
+                        <div class="row">
+                            <strong class="fs-4">Full Name</strong>
+                            <p class="py-1 text-white">Sain Firstio Ansari</p>
+                        </div>
+                        <div class="row">
+                            <strong class="fs-4">Nationality</strong>
+                            <p class="py-1 text-white">Indonesia</p>
+                        </div>
+                        <div class="row">
+                            <strong class="fs-4">Education</strong>
+                            <p class="py-1 text-white">Telkom University</p>
+                        </div>
+                        <div class="row">
+                            <strong class="fs-4">E-Mail</strong>
+                            <p class="py-1 text-white">sainfirstio27@gmail.com</p>
+                        </div>
+                    </div>
+                    <!-- <a class="btn btn-dark btn-xl" href="#services">What We Offer</a> -->
+                </div>
+                <div class="col-lg-5 col-sm-12 text-secondary">
+                    <img src="./assets/img/pic2.png" class="img-fluid working-pic" alt="pic2">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Portfolio-->
+    <section class="content-section bg-primary" id="portfolio">
+        <div class="container px-4 px-lg-5">
+            <div class="content-section-heading text-center">
+                <h2 class="text-secondary mb-5">Portfolio</h2>
+            </div>
+            <div class="row gx-0">
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#!">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <div class="h2">Landing Page</div>
+                                <span class="btn btn-warning btn-sm"><i class="fa-brands fa-html5"></i> HTML</span>
+                                <span class="btn btn-warning btn-sm"><i class="fa-brands fa-bootstrap"></i>
+                                    Bootstrap</span>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/pt1.png" alt="..." />
+                    </a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#!">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <div class="h2 text-black">Report Monitoring</div>
+                                <span class="btn btn-warning btn-sm"><i class="fa-brands fa-laravel"></i> Laravel</span>
+                                <span class="btn btn-warning btn-sm"><i class="fa-solid fa-database"></i> MySQL</span>
+                                <span class="btn btn-warning btn-sm"><i class="fa-brands fa-bootstrap"></i>
+                                    Bootstrap</span>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/pt2.png" alt="..." />
+                    </a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#!">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <div class="h2">Report Tracking Unit</div>
+                                <span class="btn btn-warning btn-sm"><i class="fa-brands fa-laravel"></i> Laravel</span>
+                                <span class="btn btn-warning btn-sm"><i class="fa-solid fa-database"></i> MySQL</span>
+                                <span class="btn btn-warning btn-sm"><i class="fa-brands fa-bootstrap"></i>
+                                    Bootstrap</span>
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/pt4.png" alt="..." />
+                    </a>
+                </div>
+                <div class="col-lg-6">
+                    <a class="portfolio-item" href="#!">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <div class="h2">Hardware & Software Installation</div>
+                                <!-- <p class="mb-0">Strawberries are such a tasty snack, especially with a little sugar on
+                                    top!</p> -->
+                            </div>
+                        </div>
+                        <img class="img-fluid" src="assets/img/pt3.png" alt="..." />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Map-->
+    <div class="content-section bg-secondary" id="contact">
+        <div class="container px-4 px-lg-5">
+            <div class="row">
+                <div class="col-lg-5 col-sm-12">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31910.913770002222!2d116.85322540086523!3d-1.2530444190400378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df146620722071f%3A0xcce4bb18cf9bd50!2sGn.%20Bahagia%2C%20Kecamatan%20Balikpapan%20Selatan%2C%20Kota%20Balikpapan%2C%20Kalimantan%20Timur!5e0!3m2!1sid!2sid!4v1695396705832!5m2!1sid!2sid" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-lg-7 col-sm-12">
+                    <form id="contactForm" method="post" action="">
+                        <div class="row align-items-stretch mb-5">
+                            <h1 class="text-primary">Lets Have a Chat!</h1>
+                            <div class="col-lg-5">
+                                <div class="mb-3">
+                                    <!-- Name input-->
+                                    <input class="form-control" id="name" name="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
+                                    <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                                </div>
+                                <div class="mb-3">
+                                    <!-- Email address input-->
+                                    <input class="form-control" id="email" name="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
+                                </div>
+                                <div class="mb-3 mb-md-0">
+                                    <!-- Phone number input-->
+                                    <input class="form-control" id="phone" name="phone" type="tel" placeholder="Your Phone *" required />
+                                    <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-5 col-sm-12">
+                                <div class="mt-1">
+                                    <!-- Message input-->
+                                    <textarea minlength="80" class="form-control" id="message" name="message" rows="5" placeholder="Your Message *" required></textarea>
+                                    <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Submit Button-->
+                        <div class="text-center">
+                            <input class="btn btn-primary btn-lg" name="submit" type="submit" Value="Send Message"></input>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer-->
+    <footer class="footer text-center bg-primary">
+        <div class="container px-4 px-lg-5">
+            <ul class="list-inline mb-5">
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white bg-secondary mr-3" href="https://github.com/Zyrtez">
+                        <i class="fa-brands fa-github"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white bg-secondary mr-3" href="https://www.instagram.com/mantau_guy/">
+                        <i class="fa-brands fa-instagram"></i></a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="social-link rounded-circle text-white bg-secondary" href="https://www.linkedin.com/in/sain-firstio-ansari-8391a3197/">
+                        <i class="fa-brands fa-linkedin"></i></a>
+                </li>
+            </ul>
+            <p class="text-muted small mb-0">Copyright &copy; Sain Firstio Ansari</p>
+        </div>
+        </div>
+        </div>
+        </div>
+    </footer>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top"><i class="fas fa-angle-up"></i></a>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
